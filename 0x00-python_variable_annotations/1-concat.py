7dd703fc520c13a2750063f2167b1f39ead53b3b
WangariNelly <nellywangari30@gmail.com>
2023-01-09 07:38:23 +0000

    String return

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -11,4 +11,3 @@ def concat(str1: str, str2: str) -> str:
         str: The concatenation of str1 and str2.
     """
     return str1 + str2
-
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Task 3 module"""
+
+
+def to_str(n: float) -> str:
+    """Return the string representation of a floar
+
+    Args:
+        n (float): The float to convert to a string.
+
+    Returns:
+        str: The string representation of n.
+    """
+    return str(n)
